444b52c62610d1a92631b6b09338f59340e4cc3e
juan pablo perera <juan.perera@bosc.solutions>
2021-01-15 19:11:44 -0600

    style Theme adjustments

--- src/components/CommitBody.js
+++ src/components/CommitBody.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import moment from 'moment'
 
 const CommitBody = (props) => {
     const { commit } = props;
+    const creationDate = (date) => {
+        var startOf = moment().isSame(date, 'day') ? "hour" : "day";
+        var formatedDate = `committed ${moment(date).startOf(startOf).fromNow()}`;
+        return formatedDate
+    }
     return (
         <tr>
             <td>
-                <div>
-                    {commit.message}
-                </div>
-                <div>
-                    {commit.author.name}
+                <a href={commit.html_url} target="_blank" rel="noreferrer" className="h4">
+                    {commit.commit.message}
+                </a>
+                <div className="text-muted">
+                    #{commit.commit.tree.sha.substring(0, 7)}&nbsp;
+                    {creationDate(commit.commit.author.date)}&nbsp;
+                        by {commit.commit.author.name}
+
                 </div>
             </td>
         </tr>
--- src/components/History.js
+++ src/components/History.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import CommitBody from './CommitBody'
+import Table from 'react-bootstrap/Table'
 
 const App = (props) => {
 
   const rendercommits = (commits) => {
-    return commits.map((commit) => <CommitBody key={commit.node_id} commit={commit.commit} />)
+    return commits.map((commit) => <CommitBody key={commit.node_id} commit={commit} />)
   }
   return (
-    <div>
-      <table>
+    <div className="col mt-4">
+      <Table bordered hover className="table-light">
         <tbody>
           {rendercommits(props.commits)}
         </tbody>
-      </table>
+      </Table>
     </div>
   )
 }
